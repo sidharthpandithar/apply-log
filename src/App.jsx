37a4e0a81c00cface179761264526c01a4dd3e59
Sidharth Pandithar <sidharth27111999@gmail.com>
2025-07-06 18:25:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Landing from "./components/Pages/Landing";
+
+export default function App() {
+  return (
+    <div className=" bg-gradient-to-r from-neutral-700 via-neutral-800 to-neutral-900 min-h-screen min-w-screen">
+      <Landing />
+    </div>
+  );
+}
